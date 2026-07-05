de032fd382d5f1054c010ae81a3b97dee4396f90
Aj-Kazza <akatia@byupathway.edu>
2026-07-05 14:49:49 +1200

    week01 home

---
+++ scripts/dates.js
@@ -0,0 +1,10 @@
+// select the DOM elements for output
+
+const year = document.querySelector("#year");
+
+// use the date object
+const today = new Date();
+
+year.innerHTML = `${today.getFullYear()} `;
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
